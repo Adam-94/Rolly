e89becc98c69bbfe57a167edd5a1cd6cdd51be84
Adam Hughes <ashughes07@aol.com>
2020-09-24 19:04:19 +0000

    Fixed search function

--- search.py
+++ search.py
@@ -3,22 +3,20 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from PIL import Image
 
-import os
+import time
 
 def screenshot(message):
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--start-maximized')
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
+    driver = webdriver.Chrome(options=options)
     driver.get("https://radai.github.io/dnd5tools/?q="+message)
+    time.sleep(0.5)
 
     original_size = driver.get_window_size()
     required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
     required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
 
     driver.set_window_size(required_width, required_height)
-    element = driver.find_element_by_css_selector("#results")
-    screen_shot = element.screenshot_as_base64()
+    driver.find_element_by_css_selector("#results").screenshot("screenshot.png")  # avoids scrollbar
     driver.set_window_size(original_size['width'], original_size['height'])
-
-    return screen_shot
